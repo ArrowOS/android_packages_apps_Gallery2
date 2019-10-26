liu-wanshun/Gallery2

Move Gallery2 and its libs to /product
This app is frequently overriden by proprietary apps
on shipping devices, so move it to /product along with
its jni libs.

Bug: 122353713
Test: launch gallery on aosp_arm64 emulator
Change-Id: I5f7ae53ef41a26c1b89b93cdae24178c2a99e261

CleanSpec.mk
@@ -50,7 +50,10 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
-
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Gallery2)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_jpegstream.so)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_eglfence.so)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_filtershow_filters.so)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************

ArrowOS/android_packages_apps_Gallery2

CleanSpec.mk
@@ -50,7 +50,10 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Gallery*)
-
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Gallery2)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_gallery_jpegstream.so)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_gallery_eglfence.so)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libjni_gallery_filters.so)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************